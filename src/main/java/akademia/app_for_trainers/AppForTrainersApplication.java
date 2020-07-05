955cc7e9e01220bb4d738e4bb68d08444c34bcf2
Jakub Starzyński <starzynski96@gmail.com>
2020-07-05 12:29:55 +0200

    Add new files

---
+++ src/main/java/akademia/app_for_trainers/AppForTrainersApplication.java
@@ -0,0 +1,13 @@
+package akademia.app_for_trainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForTrainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppForTrainersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/akademia/app_for_trainers/AppForTrainersApplicationTests.java
@@ -0,0 +1,13 @@
+package akademia.app_for_trainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppForTrainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
